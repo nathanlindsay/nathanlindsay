036b61b16cf7da54c3e6d302015b8b5fb287d4f6
Nathan Lindsay <993035+nathanlindsay@users.noreply.github.com>
2022-05-19 14:33:06 +0100

    Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        isabelline: "#F4EBE8",
+        jet: "#2D2D2A",
+      },
+    },
+  },
+  plugins: [],
+};
